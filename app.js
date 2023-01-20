f028c22a4c3de7eb1ae3a1de1aecd4d711d3d3ed
Russell Oyama <russelloyama@gmail.com>
2023-01-20 07:14:16 -1000

    store data from post from into post object

--- app.js
+++ app.js
@@ -31,8 +31,13 @@ app.get('/compose', function(req, res){
 });
 
 app.post('/compose', function(req, res){
-  const postTitle = req.body.postTitle;
-  console.log(postTitle);
+
+  const post = {
+    title : req.body.postTitle,
+    content :  req.body.postBody
+  };
+  
+  console.log(post);
 });
 
 
