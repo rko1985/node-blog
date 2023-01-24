c0feee98c83778e32a8316724a1b1c411442cba4
Russell Oyama <russelloyama@gmail.com>
2023-01-24 07:37:37 -1000

    passing posts to home route

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.set('view engine', 'ejs');
 let posts = [];
 
 app.get('/', function(req, res){
-  res.render('home', {startingContent: homeStartingContent});
-  console.log(posts);
+  res.render('home', {startingContent: homeStartingContent, posts: posts});
+  console.log(posts)  ;
 });
 
 app.get('/about', function(req, res){
