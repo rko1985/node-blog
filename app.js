f442551f8299746461086554274aef46933a2bd3
Russell Oyama <russelloyama@gmail.com>
2023-01-27 06:52:05 -1000

    checking if posts route matched title in posts array

--- app.js
+++ app.js
@@ -45,7 +45,16 @@ app.post('/compose', function(req, res){
 });
 
 app.get('/posts/:postName', function(req,res){
-  console.log(req.params.postName);
+  const requestedTitle = req.params.postName;
+  
+  posts.forEach(function(post){
+    const storedTitle = post.title;
+    
+    if(storedTitle === requestedTitle){
+      console.log('match found!');
+    }
+  });
+
 });
 
 
